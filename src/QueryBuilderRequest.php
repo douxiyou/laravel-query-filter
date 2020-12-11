6de06793773b7845a3b7c8e589cf5fc7d7543288
sherrin.ning <sherrin.ning@gmail.com>
2020-12-11 10:51:10 +0800

    init

---
+++ src/QueryBuilderRequest.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Http\Request;
+
+class QueryBuilderRequest extends Request
+{
+    /**
+     * @param Request $request
+     * @return static
+     */
+    static function fromRequest(Request $request): self {
+        return static::createFrom($request, new self());
+    }
+}
